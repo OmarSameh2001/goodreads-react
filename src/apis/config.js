7ab4af95e930ed30beea985d966d833ff8d81c91
Hoda Magdy <hmag425@gmail.com>
2025-02-02 22:52:10 +0200

    adding home first components

---
+++ src/apis/config.js
@@ -0,0 +1,17 @@
+// import axios from "axios";
+// 
+// const axiosInstance = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL ,
+//   posterURL: import.meta.env.VITE_API_POSTER_URL 
+// });
+// 
+// axiosInstance.interceptors.request.use(function (config) {
+//   config.headers.Authorization = import.meta.env.VITE_API_TOKEN;
+//   config.headers.Accept ="application/json";
+    // return config;
+//   },function (error) {
+    // return Promise.reject(error);
+//   }
+// );
+// 
+// export default axiosInstance;
